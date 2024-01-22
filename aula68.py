90d3ee125c836e38407bf24d2aa5d7f91987d86b
Leonardo Silva <leonardo_silvasoares98@hotmail.com>
2024-01-22 18:49:34 -0300

    Escopo de funções e módulo em Python

--- aula68.py
+++ aula68.py
@@ -6,3 +6,24 @@ O escopo global é o escopo onde todo o código é alcançavel.
 O escopo local é o escopo onde apenas nomes da mesmo local
 podem ser alcançados.
 """
+
+x = 1
+
+
+def escopo():
+    global x
+    x = 10
+
+    def outra_funcao():
+        global x
+        x = 11
+        y = 2
+        print(x, y)
+
+    outra_funcao()
+    print(x)
+
+
+print(x)
+escopo()
+print(x)
