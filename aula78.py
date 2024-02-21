33ec23be7be50c9f081666f172e57a51f1907c59
Leonardo Silva <leonardo_silvasoares98@hotmail.com>
2024-02-21 10:39:49 -0300

    Update aula78.py

--- aula78.py
+++ aula78.py
@@ -42,3 +42,4 @@ print(s1)
 # intersecção & (intersection) - Itens presentes em ambos
 # diferença - Itens presentes apenas no set da esquerda
 # diferença simétrica ^ - Itens que não estão em ambos
+
