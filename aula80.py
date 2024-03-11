7970c0c4973b411acf40bb829eca0164a7327b11
Leonardo Silva <leonardo_silvasoares98@hotmail.com>
2024-03-10 23:25:21 -0300

    priemiro duplicado considerando a segunda ocorrência

--- aula80.py
+++ aula80.py
@@ -26,20 +26,24 @@ lista_de_listas_de_inteiros = [
     [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
 ]
 
-def find_first_duplicate(nubmbrs):
-    # Cria um conjunto vazio para armazenar os nùmeros vistos
-    seen_numbrs = set()
 
-    # Itera sobre os números da lisra
-    for number in number:
-        # Se número já estiver no cojunto, retorna-a a duplicação
-        if number in seen_numbrs:
-            return number
-        # Adiciona o número ao conjunto 
-        seen_numbrs.add(number)
-    
-    # Se nenhum duplicação for encontrada, retorna -1
-    return -1
-    print(find_first_duplicate([1, 2, 3, 3, 2, 1])) # returns 1
-    print(find_first_duplicate([1, 2, 3, 4, 5, 6]))  # returns -1
-    print(find_first_duplicate([1, 4, 9, 8, 9, 4, 8]))  # returns 9
+def encontra_primeiro_duplicado(lista_de_inteiros):
+    numeros_checados = set()
+    primeiro_duplicado = -1
+
+    for numero in lista_de_inteiros:
+        if numero in numeros_checados:
+            primeiro_duplicado = numero
+            break
+
+        numeros_checados.add(numero)
+
+    return primeiro_duplicado
+
+
+for lista in lista_de_listas_de_inteiros:
+    print(
+        lista,
+        encontra_primeiro_duplicado(lista)
+    )
+
