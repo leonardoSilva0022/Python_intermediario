d8aef360b5591c667edfbdb070239745ca0f1d9f
Leonardo Silva <leonardo_silvasoares98@hotmail.com>
2024-03-13 15:58:17 -0300

    Create aula83.py

---
+++ aula83.py
@@ -0,0 +1,20 @@
+# Empacotamento e desempacotamento de dicionários
+a, b = 1, 2
+a, b = b, a
+# print(a, b)
+
+pessoa = {
+    'nome': 'Igor',
+    'sobrenome': 'Gomes',
+}
+(a1, a2), (b1, b2) = pessoa.items()
+print(a1, a2)
+print(b1, b2)
+
+for chave, valor in pessoa.items():
+    print(chave, valor)
+
+# args e kwargs
+# args (já vimos)
+# kwargs - keyword arguments (argumentos nomeados)
+    
