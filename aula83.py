80a4a4402d51f8bc2bf331d280b1eab07f36dac9
Leonardo Silva <leonardo_silvasoares98@hotmail.com>
2024-03-17 00:05:52 -0300

    Empacotamento e desempacotamento de dicionários + *args e **kwargs

--- aula83.py
+++ aula83.py
@@ -3,19 +3,46 @@ a, b = 1, 2
 a, b = b, a
 # print(a, b)
 
+
+# (a1, a2), (b1, b2) = pessoa.items()
+# print(a1, a2)
+# print(b1, b2)
+
+# for chave, valor in pessoa.items():
+#     print(chave, valor)
+
 pessoa = {
     'nome': 'Igor',
     'sobrenome': 'Gomes',
 }
-(a1, a2), (b1, b2) = pessoa.items()
-print(a1, a2)
-print(b1, b2)
 
-for chave, valor in pessoa.items():
-    print(chave, valor)
+dados_pessoas = {
+    'idade': 16,
+    'altura': 1.6,
+}
+
+pessoas_completa = {**pessoa, **dados_pessoas}
+# print(pessoa, dados_pessoas)
 
 # args e kwargs
 # args (já vimos)
 # kwargs - keyword arguments (argumentos nomeados)
-    
-    
+
+
+def mostro_argumentos_nomeados(*args, **kwargs):
+    print('NÃO NOMEADOS:', args)
+
+    for chave, valor in kwargs.items():
+        print(chave, valor)
+
+
+# mostro_argumentos_nomeados(nome='Michaela', qlq=123)
+# mostro_argumentos_nomeados(**pessoas_completa)
+        
+configuracoes = {
+    'arg1': 1,
+    'arg2': 2,
+    'arg3': 3,
+    'arg4': 4,
+}
+mostro_argumentos_nomeados(**configuracoes)
