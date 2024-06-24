5f69abf7bf992dae4741dc0d4feb52f7fda78260
Leonardo Silva <leonardo_silvasoares98@hotmail.com>
2024-06-24 01:16:53 -0300

    .

--- aula99.py
+++ aula99.py
@@ -12,4 +12,11 @@
 # print(modulo.soma_do_modulo(1, 2))
 # print(variavel)
 # # print(nova_variavel)
-print(__name__)
+# from aula99_package.modulo import soma_do_modulo, fala_oi
+
+# print(__name__)
+# fala_oi()
+
+import aula99_package
+
+print(aula99_package)
--- aula99_package/modulo.py
+++ aula99_package/modulo.py
@@ -1,8 +1,9 @@
-__all__ = [
-    'variavel',
-    'soma_do_modulo',
-    'nova_variavel'
-]
+# __all__ = [
+#     'variavel',
+#     'soma_do_modulo',
+#     'nova_variavel'
+# ]
+from aula99_package.modulo_b import fala_oi
 
 variavel = 'Alguma coisa'
 
@@ -12,3 +13,4 @@ def soma_do_modulo(x, y):
 
 
 nova_variavel = 'OK'
+# fala_oi()
