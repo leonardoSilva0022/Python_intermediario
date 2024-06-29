36c06e2c4fdd091acb18579f45e39827903bba64
Leonardo Silva <leonardo_silvasoares98@hotmail.com>
2024-06-29 01:46:57 -0300

    Create aula102.py

---
+++ aula102.py
@@ -0,0 +1,17 @@
+# Variáeis lire + nonlocal (locals, globals)
+print(globals())
+# def fora(x):
+#     a = x
+
+#     def dentro():
+#         print(locals())
+#         print(dentro.__code__.co_freevars)
+#         return a
+#     return dentro
+
+
+# dentro1 = fora(10)
+# dentro2 = fora(20)
+
+# print(dentro1())
+# print(dentro2())
