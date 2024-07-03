454f064a8eebdf82c0dc8da8302ddafa92e69360
Leonardo Silva <leonardo_silvasoares98@hotmail.com>
2024-07-02 23:27:17 -0300

    Update aula102.py

--- aula102.py
+++ aula102.py
@@ -4,7 +4,7 @@
 #     a = x
 
 #     def dentro():
-#         print(locals())
+#         # print(locals())
 #         print(dentro.__code__.co_freevars)
 #         return a
 #     return dentro
@@ -15,3 +15,19 @@
 
 # print(dentro1())
 # print(dentro2())
+def concatenar(string_inicial):
+    valor_final = string_inicial
+
+    def interna(valor_a_concaternar=''):
+        nonlocal valor_final
+        valor_final += valor_a_concaternar
+        return valor_final
+    return interna
+
+
+c = concatenar('a')
+print(c('b'))
+print(c('c'))
+print(c('d'))
+final = c()
+print(final)
