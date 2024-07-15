086455bb20ec28b596462e4cde3a30e3af2098af
Leonardo Silva <leonardo_silvasoares98@hotmail.com>
2024-07-14 21:24:46 -0300

    os

--- aula116.py
+++ aula116.py
@@ -1,3 +1,4 @@
+import os
 # Criando arquivos com Python + Context Manager with
 # Usamos a função open para abrir
 # um arquivo em Python (ele pode ou não existir)
@@ -18,14 +19,45 @@
 # Vamos falar mais sobre o módulo json, mas:
 # json.dump = Gera um arquivo json
 # json.load
-caminho_arquivo = '\\Users\\leona\\OneDrive\\Documentos\\Python Intermediário\\'
 caminho_arquivo = 'aula116.txt'
 
 # arquivo = open(caminho_arquivo, 'w')
 # #
 # arquivo.close()
-with open(caminho_arquivo, 'w') as arquivo:
-    print('Olá mundo')
-    print('Arquivo vai ser fechado')
+
+# with open(caminho_arquivo, 'w+') as arquivo:
+#     arquivo.write('Linha 1\n')
+#     arquivo.write('Linha 2\n')
+#     arquivo.writelines(
+#         ('Linha 3\n', 'Linha 4\n')
+#     )
+#     arquivo.seek(0, 0)
+#     print(arquivo.read())
+#     print('Lendo ')
+#     arquivo.seek(0, 0)
+#     print(arquivo.readline(), end='')
+#     print(arquivo.readline().strip())
+#     print(arquivo.readline().strip())
+
+#     print('READLINES')
+#     arquivo.seek(0, 0)
+#     for linha in arquivo.readlines():
+#         print(linha.strip())
+
+
+# print('#' * 10)    
+
+# with open(caminho_arquivo, 'r') as arquivo:
+#     print(arquivo.read())
+
+with open(caminho_arquivo, 'w', encoding='utf-8') as arquivo:
+    arquivo.write('Atenção\n')
+    arquivo.write('Linha 1\n')
+    arquivo.write('Linha 2\n')
+    arquivo.writelines(
+        ('Linha 3\n', 'Linha 4\n')
+    )
     
-    
+
+# os.unlink(caminho_arquivo)
+# os.rename(caminho_arquivo, 'aula116-2.txt')
