13ba29221f5fd864573614337b5f7612e38b2a3f
Leonardo Silva <leonardo_silvasoares98@hotmail.com>
2024-08-20 12:24:57 -0300

    Método em instância de classe Python

--- aula121.py
+++ aula121.py
@@ -1,15 +1,12 @@
 # Métodos em instância de classes Python
-# Hard coded - É algo que foi escrito diretamente no código
-class Carro: 
+#Hard coded - É algo que foi escrito diretamente no código
+class Carro:
     def __init__(self, nome):
         self.nome = nome
-    
+
     def acelerar(self):
         print(f'{self.nome} está acelerando...')
 
-
-string = 'Leonardo'
-print(string.upper())
 
 fusca = Carro('Fusca')
 print(fusca.nome)
